9931af66ae8b0e36349c190838a8a4bcbd7ed31f
Sindhu Giri <sindhgir@umich.edu>
2017-04-09 10:04:46 -0400

    first commit

---
+++ 206_project_plan.py
@@ -0,0 +1,24 @@
+## Your name: Sindhu Giri 
+## The option you've chosen: Option 2
+
+# Put import statements you expect to need here!
+import requests_oauthlib 
+import webbrowser
+import json
+import unittest
+import itertools
+import collections
+import tweepy
+import twitter_info 
+import sqlite3
+
+
+
+
+
+
+
+# Write your test cases here.
+
+
+## Remember to invoke all your tests...
