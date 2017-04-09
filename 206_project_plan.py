ca955949be98e1a9d19b5a321b87490e79ee76f8
Sindhu Giri <sindhgir@umich.edu>
2017-04-09 19:10:06 -0400

    Second commit

--- 206_project_plan.py
+++ 206_project_plan.py
@@ -14,11 +14,43 @@ import sqlite3
 
 
 
-
-
-
-
 # Write your test cases here.
+class Tests(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(type(cached_data), type ({}))
+    def test_2(self):
+        self.assertEqual(len(b["user"])>0, True)
+    def test_3(self):
+        
+    def test4(self):
+        self.assertEqual(type(rating_info), type([]))
+    def test5(self):
+        self.assertEqual(len("date_pair")> 1, True)
+    def test6(self):
+        self.assertEqual(type(s["comments"]["data"]), type([])
+    def test7(self):
+    	self.assertEqual(
+    def test8(self):
+    	self.assertEqual()
 
 
-## Remember to invoke all your tests...
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
+
+## Remember to invoke all your tests..
+test_1()
+test_2() 
+test_3()
+test_4()
+test_5()
+test_6()
+test_7()
+test_8()
+
+
+
+
+
+
+
